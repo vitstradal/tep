e7339983e07f811f23a8e89fc7d8c71437a31592
Vitas Stradal <vitas@matfyz.cz>
2013-11-02 17:18:57 +0100

    preklady a ladeni

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from CanCan::AccessDenied do |exception|
+      sign_out :user
+      redirect_to :new_user_session, :alert => exception.message
+  end
 end
