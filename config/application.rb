21d902dfa8b09a6e8ff58c9649c03a9b29d2329b
Vitas Stradal <vitas@matfyz.cz>
2013-11-03 14:49:12 +0100

    conf app

--- config/application.rb
+++ config/application.rb
@@ -40,9 +40,8 @@ module Pia
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    config.action_mailer.default_url_options = { :host => 'pikomat.ms.mff.cuni.cz/provedouci/rails/pia' }
+    config.action_mailer.default_url_options = { :host => 'pikomat.ms.mff.cuni.cz/pia' }
     config.action_mailer.delivery_method = :sendmail
-    config.ble = "ahoj"
 
   end
 end
