8662b2b4acf46199e9b70f2596bb62489264efa0
Viťas Strádal <vitas@matfyz.cz>
2013-11-07 23:02:37 +0100

    fix school

--- app/models/sosna/school.rb
+++ app/models/sosna/school.rb
@@ -1,7 +1,7 @@
 class Sosna::School < ActiveRecord::Base
   has_many :solvers
   validates :name, :psc, :city, presence: true
-  validates_uniqueness_of :short
+  #validates_uniqueness_of :short
   def address
      "#{street} #{num}, #{psc} #{city}"
   end
