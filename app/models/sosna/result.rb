c57b2558bba9b74023fc692713d179278a6de4f6
Vitas Stradal <vitas@matfyz.cz>
2014-03-30 09:20:33 +0200

    fixy vysledkovek

--- app/models/sosna/result.rb
+++ app/models/sosna/result.rb
@@ -1,4 +1,12 @@
 class Sosna::Result < ActiveRecord::Base
   include SosnaHelper
   belongs_to :solver
+
+  def rank_multi?
+    return rank < rank_to
+  end
+
+  def class_rank_multi?
+    return class_rank < class_rank_to
+  end
 end
