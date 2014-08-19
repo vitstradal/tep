20c5c92b0b7e099c466c9cf3b50e28ace6d9a5e9
Viťas Strádal <vitas@matfyz.cz>
2014-08-19 09:21:47 +0200

    seance vyprsi za 60 minut

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         #:token_authenticatable, 
+         #:token_authenticatable,
          :confirmable, :lockable, :timeoutable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :timeout_in => 60.minutes
+
 
   # Setup accessible (or protected) attributes for your model
   #attr_accessible :email, :password, :password_confirmation, :remember_me
