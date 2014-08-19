1cc66aac8639f3f7020e22e55ddcaed95dd1159d
Viťas Strádal <vitas@matfyz.cz>
2014-08-19 08:53:33 +0200

    session cookie obsahuj env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-#Tep::Application.config.session_store :cookie_store, :key => '_tep_session'
+Tep::Application.config.session_store :cookie_store, :key => '_tep_session_' + Rails.env
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Tep::Application.config.session_store :active_record_store
+#Tep::Application.config.session_store :active_record_store
+
