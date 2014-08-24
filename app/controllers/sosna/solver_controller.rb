201882e5c09206a2fc824a764c2b98d0a4597c96
Vitas Stradal <vitas@matfyz.cz>
2014-08-24 08:33:46 +0200

    fix: datum nemuze byt prazdne (pro anonyma)

--- app/controllers/sosna/solver_controller.rb
+++ app/controllers/sosna/solver_controller.rb
@@ -43,13 +43,13 @@ class Sosna::SolverController < SosnaController
 
     solver = Sosna::Solver.new(params[:sosna_solver])
 
-
     solver.valid?
     agree = ! params[:souhlasim].nil?
     solver.errors.add(:souhlasim, 'Je nutno souhlasit s podmínkami') if ! agree
     solver.errors.add(:email, 'je již registrován u jiného řešitele') if Sosna::Solver.where(email: solver.email, annual: @annual).exists?
     solver.errors.add(:email, 'neexistující adresa') if !solver.email.empty? && !email_valid_mx_record?(solver.email)
     solver.errors.add(:birth, 'jsi příliš stár') if solver.errors[:birth].blank? && !solver.birth.empty? && (Date.parse(solver.birth) + 17.years) < Date.today
+    solver.errors.add(:birth, 'nemůže být prázdné') if solver.errors[:birth].blank? && ( current_user.nil? ||  !current_user.admin?  )
 
     case school_id
      when 'none'
