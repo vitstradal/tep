a9d32c2071480475ffe6d2aac26a2b8ddbe962ea
Vitas Stradal <vitas@matfyz.cz>
2014-08-24 08:48:08 +0200

    session cookie: secure

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Tep::Application.config.session_store :active_record_store, :key => '_tep_session_' + Rails.env
+Tep::Application.config.session_store :active_record_store, :key => '_tep_session_' + Rails.env, :secure => true
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
