d9cf138c09fca9b84446256aa6f4cf2b25552d6c
Viťas Strádal <vitas@matfyz.cz>
2014-12-06 07:10:54 +0100

    fixy kolem cache

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,6 +3,5 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register "text/plain", :pik
-# rozbije to editaci protoze tam je page.wiki
-#Mime::Type.register "text/plain", :wiki
+Mime::Type.register "text/plain; charset=UTF-8", :pik
+Mime::Type.register "text/plain; charset=UTF-8", :wiki
