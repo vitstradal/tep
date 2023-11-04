380a5bee8a5cd33ec15e2268e177ae725e297db7
hejnya <antonin.hejny@gmail.com>
2023-11-04 16:22:46 +0100

    basic prihlasovani na akce

---
+++ app/controllers/event_participants_controller.rb
@@ -0,0 +1,26 @@
+class EventParticipantsController < ApplicationController
+require 'pp'
+  def update
+    @participant = EventParticipant.find_by("event_id=? AND user_id=?", params[:event_id], params[:user_id])
+    
+    if @participant.update(participant_params)
+      redirect_to edit_participants_event_path(params[:event_id])
+    else
+      render "events/edit_participants", status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @participant = EventParticipant.find_by("event_id=? AND user_id=?", params[:event_id], params[:user_id])
+    pp @participant
+    @participant.destroy
+
+    redirect_to event_path(params[:event_id])
+  end
+
+  private
+    def participant_params
+      params.require(:event_participant).permit(:status, :note)
+    end
+end
+
